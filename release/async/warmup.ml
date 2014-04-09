5961b5a9a8ead4fadfa6aaad7666b523ee68c4a3
Ethan Rubinson <ethanrubinson@yahoo.com>
2014-04-09 10:28:30 -0400

    Implemented deferred_map (Ex. #2)

--- release/async/warmup.ml
+++ release/async/warmup.ml
@@ -1,8 +1,14 @@
+open Async.Std
 
 let fork d f1 f2 =
   failwith "My mind is aglow with whirling, transient nodes of thought..."
 
+
 let deferred_map l f =
-  failwith "Ditto."
-
-
+  let rec map_loop = function
+    | [] ->  return []
+    | h_def::tl_def -> h_def >>= fun h ->
+					map_loop tl_def >>= fun tl ->
+					return (h::tl)
+  in
+   map_loop (List.map (fun x -> f x) l)
