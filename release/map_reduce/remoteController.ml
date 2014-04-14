ef3a47cfdd3b6423901f6e72c393517eecf23e2a
Ethan Rubinson <ethanrubinson@yahoo.com>
2014-04-13 23:54:18 -0400

    Further work on RemoteController map_reduce
    Not sure if we should be using a list or a aQueue, but now the workers
    receive their job name.

--- release/map_reduce/remoteController.ml
+++ release/map_reduce/remoteController.ml
@@ -1,5 +1,5 @@
 open Async.Std
-open Async_extra
+open Protocol
 
 let ips = ref []
 
@@ -10,39 +10,33 @@ let init addrs =
 module Make (Job : MapReduce.Job) = struct
   let map_reduce inputs =
 
-  	let worker_list = !ips in
-  	let rec connect_to_workers = function
-  		| [] -> return []
-  		| h::t -> (Tcp.connect (Tcp.to_host_and_port (fst h) (snd h) )) >>= fun worker ->  
-  				connect_to_workers t >>= fun rest_of_workers ->
-  				return (worker :: rest_of_workers);
-  	in
-  	let connected_workers = connect_to_workers worker_list in
+    let worker_list = !ips in
+    let rec connect_and_initialize_workers = function
+      | [] -> return []
+      | h::t -> (Tcp.connect (Tcp.to_host_and_port (fst h) (snd h) )) >>= fun connection ->
+          let (_,_,writer) = connection in
+          (Writer.write writer Job.name);
+          connect_and_initialize_workers t >>= fun conn_list_tl ->
+          return (connection :: conn_list_tl)
+    in
+    connect_and_initialize_workers worker_list >>= fun conn_list ->
 
-  	let workerreq = WorkerRequest(Job) in 
-  	let workerres = WorkerResponse(Job) in
+    (*At this point we should have a list of connections to workers and they all have been sent
+       the name of the job they are to be executing *)
 
-  	Job.name
+      let module WReq = WorkerRequest(Job) in
+      let module WRes = WorkerResponse(Job) in
+      failwith "Not Implemented"
 
-  	(*At this point we should have a list of workers we are connected
-  	  to in the variable connected_workers*)
+  (* Parse the input data and initialize a map job for the workers with the parsed input *)
 
-	(*Create a job based on the input*)
+  (* Wait for all the workers to finish, putting their data into a aQueue, and combine the intermediary data with the same keys *)
 
-	(*Send a message to all of the workers with the name of the job to complete*)
+  (* Parse the aggregated intermediate data and initialize a reduce job for the workers with the parsed input*)
 
-	(* Parse the input data and initialize a map job for the workers with the parsed input *)
+  (* Wait for all of the reduce workers to finish *)
 
-	(* Wait for all the workers to finish, putting their data into a aQueue, and combine the intermediary data with the same keys *)
-
-	(* Parse the aggregated intermediate data and initialize a reduce job for the workers with the parsed input*)
-
-	(* Wait for all of the reduce workers to finish *)
-
-	(* Combine the reduce results, and return to the user *)
-
-
-    failwith "Nowhere special."
-
+  (* Combine the reduce results, and return to the user *)
+  
 end
 
