5112b09707a51c03833644ec2779be2cbec69f9c
Ethan Rubinson <ethanrubinson@yahoo.com>
2014-04-14 23:38:14 -0400

    Continued implementation of RemoteController
    Finished mapping section and got intermediate data back from workers

--- release/map_reduce/remoteController.ml
+++ release/map_reduce/remoteController.ml
@@ -26,6 +26,37 @@ module Make (Job : MapReduce.Job) = struct
 
       let module WReq = WorkerRequest(Job) in
       let module WRes = WorkerResponse(Job) in
+
+      let rec initial_mapping input_list worker_list = match input_list with
+        | [] -> []
+        | h::t -> (match worker_list with
+              | [] -> failwith "[Error] No workers!"
+              | h'::t' -> (let (_,r,w) = h' in
+                     (WReq.send w (WReq.MapRequest h) );
+                     (h, h', (WRes.receive r)) :: (initial_mapping t (t' @ [h']))))
+      in 
+      let started_jobs = initial_mapping inputs conn_list in
+      
+      let rec wait_for_completion running_jobs alive_workers = match running_jobs with
+        | [] -> return []
+        | h::t -> (let (job,conn,result) = h in
+              result >>= fun res ->
+                match res with
+                 | `Eof -> ((print_endline "[Warning] Socket closed, worker presumed to be down; Restarting job");
+                      let new_alive_workers = List.filter (fun p -> (p != conn)) alive_workers in
+                      match new_alive_workers with
+                        | [] -> failwith "[Error] All workers have died!"
+                        | h'::t' -> (let (_,r,_) = h' in (wait_for_completion (t @ [(job,h',(WRes.receive r))]) (t' @ [h']))) 
+                      )
+                 | `Ok map_result -> (
+                  match map_result with 
+                    | WRes.JobFailed(s) -> failwith ("[Error]: Got 'JobFailed' from worker with message: " ^ s)
+                      | WRes.MapResult(m) -> (wait_for_completion t alive_workers) >>= fun data -> return (m :: data)
+                      | WRes.ReduceResult(_) -> failwith "[Error]: Got 'ReduceResult' from worker; Expected 'MapResult'"
+                 ))
+      in 
+      ignore(wait_for_completion started_jobs conn_list);
+
       failwith "Not Implemented"
 
   (* Parse the input data and initialize a map job for the workers with the parsed input *)
