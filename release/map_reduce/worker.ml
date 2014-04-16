f5f3540e7460ab5f380731046f15906f8efa667b
Ethan Rubinson <ethanrubinson@yahoo.com>
2014-04-16 16:17:37 -0400

    Formatting
    Sorry Kate, I couldn’t resist =D

--- release/map_reduce/worker.ml
+++ release/map_reduce/worker.ml
@@ -1,27 +1,33 @@
 open Async.Std
+open Protocol
 
 module Make (Job : MapReduce.Job) = struct
 
-  (* see .mli *)
-      let rec run r w =
+  let rec run r w =
      let module WorkerReq = WorkerRequest(Job) in
      let module WorkerRes = WorkerResponse(Job) in
-     WorkerReq.receive r>>= fun req ->
-     match req with
-     |`Eof -> (try_with (fun () -> return (WorkerRes.send w (WorkerRes.JobFailed("`Eof in worker - receive")))) >>= 
-     			fun res -> match res with
-     			|Core.Std.Result.Error e -> failwith "writer failed in sending job_failed"
-     			|Core.Std.Result.Ok v -> run r w)
-     |`Ok(WorkerReq.MapRequest(i)) -> 
-                (Job.map i >>= fun res -> try_with (fun () -> return (WorkerRes.send w (WorkerRes.MapResult(res)))) >>=
-                 fun res1 -> match res1 with
-     			|Core.Std.Result.Error e -> failwith "writer failed in sending MapResult"
-     			|Core.Std.Result.Ok v -> run r w)
-     |`Ok(WorkerReq.ReduceRequest(k,lst)) -> 
-                (Job.reduce (k,lst) >>= fun res -> try_with (fun () -> return (WorkerRes.send w (WorkerRes.ReduceResult(res)))) >>=
-                 fun res1 -> match res1 with
-     			|Core.Std.Result.Error e -> failwith "writer failed in sending ReduceResult"
-     			|Core.Std.Result.Ok v -> run r w)
+     WorkerReq.receive r
+     >>= fun req ->
+      match req with
+        | `Eof -> (try_with (fun () -> return (WorkerRes.send w (WorkerRes.JobFailed("`Eof in worker - receive"))))
+                  >>= fun res -> 
+                    match res with
+     			            |Core.Std.Result.Error e -> failwith "writer failed in sending job_failed"
+     			            |Core.Std.Result.Ok v -> run r w)
+        | `Ok (WorkerReq.MapRequest(i)) -> 
+          (Job.map i >>= fun res -> 
+            try_with (fun () -> return (WorkerRes.send w (WorkerRes.MapResult(res))))
+            >>= fun res1 -> 
+              match res1 with
+     			      |Core.Std.Result.Error e -> failwith "writer failed in sending MapResult"
+     			      |Core.Std.Result.Ok v -> run r w)
+        | `Ok (WorkerReq.ReduceRequest(k,lst)) -> 
+                (Job.reduce (k,lst) >>= fun res -> 
+                  try_with (fun () -> return (WorkerRes.send w (WorkerRes.ReduceResult(res))))
+                  >>= fun res1 -> 
+                    match res1 with
+     			            |Core.Std.Result.Error e -> failwith "writer failed in sending ReduceResult"
+     			            |Core.Std.Result.Ok v -> run r w)
 
 end
 
